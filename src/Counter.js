8fe71921e3c3235e2e2a117106ba6af2bb760466
Marcio Flavio <mflaviof1995@gmail.com>
2022-06-19 14:07:41 -0300

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+export const Counter = () => {
+  const count = 0;
+
+  return (
+    <>
+      <p>{count}</p>
+      <button>Decrement</button>
+      <button>Increment</button>
+    </>
+  );
+};
+
+export default Counter;
